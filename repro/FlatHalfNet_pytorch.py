34ccf47677fa966ea6dd291d70b315aea6dd564f
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-02-14 04:05:20 +0100

    debug

--- repro/FlatHalfNet_pytorch.py
+++ repro/FlatHalfNet_pytorch.py
@@ -72,5 +72,9 @@ def dummy_train(use_autocast=True, device=torch.device("cpu")):
 
 if __name__ == "__main__":
     # Compare the CPU usage between these two:
-    # dummy_train(use_autocast=True, device=torch.device("cuda", 0))
+    dummy_train(use_autocast=True)
+    dummy_train(use_autocast=True)
+
+    # And the GPU version:
+    dummy_train(use_autocast=True, device=torch.device("cuda", 0))
     dummy_train(use_autocast=False, device=torch.device("cuda", 0))
