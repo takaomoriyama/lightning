5946520fe41d6a6efb7abd05ac6cfe51fbf81015
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-02-14 03:42:08 +0100

    debug

--- repro/FlatHalfNet_pytorch.py
+++ repro/FlatHalfNet_pytorch.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     val_dataset = DataComponents.Val_Dataset('datasets/val/img',
                                              'datasets/val/lab',)
     trainer = pl.Trainer(max_epochs=100, log_every_n_steps=1, logger=False,
-                         accelerator="cpu", devices=1, enable_checkpointing=False,
+                         accelerator="gpu", devices=1, enable_checkpointing=False,
                          precision=16, gradient_clip_val=0.5,)
     model = HalfNetPL()
     trainer.fit(model, train_dataloaders=train_loader)
