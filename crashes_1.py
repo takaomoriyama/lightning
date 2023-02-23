aa26dac496384d3427bdfe9ce0909b54ec73f068
awaelchli <aedu.waelchli@gmail.com>
2023-02-23 14:57:09 +0100

    orig

---
+++ crashes_1.py
@@ -0,0 +1,303 @@
+
+import os
+import os.path as op
+import time
+
+from datasets import load_dataset
+from lightning import Fabric
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+import torchmetrics
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from watermark import watermark
+
+import os
+import sys
+import tarfile
+import time
+
+import numpy as np
+import pandas as pd
+from packaging import version
+from torch.utils.data import Dataset
+from tqdm import tqdm
+import urllib
+
+
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = progress_size / (1024.0**2 * duration)
+    percent = count * block_size * 100.0 / total_size
+
+    sys.stdout.write(
+        f"\r{int(percent)}% | {progress_size / (1024.**2):.2f} MB "
+        f"| {speed:.2f} MB/s | {duration:.2f} sec elapsed"
+    )
+    sys.stdout.flush()
+
+
+def download_dataset():
+    source = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+    target = "aclImdb_v1.tar.gz"
+
+    if os.path.exists(target):
+        os.remove(target)
+
+    if not os.path.isdir("aclImdb") and not os.path.isfile("aclImdb_v1.tar.gz"):
+        urllib.request.urlretrieve(source, target, reporthook)
+
+    if not os.path.isdir("aclImdb"):
+
+        with tarfile.open(target, "r:gz") as tar:
+            tar.extractall()
+
+
+def load_dataset_into_to_dataframe():
+    basepath = "aclImdb"
+
+    labels = {"pos": 1, "neg": 0}
+
+    df = pd.DataFrame()
+
+    with tqdm(total=50000) as pbar:
+        for s in ("test", "train"):
+            for l in ("pos", "neg"):
+                path = os.path.join(basepath, s, l)
+                for file in sorted(os.listdir(path)):
+                    with open(os.path.join(path, file), "r", encoding="utf-8") as infile:
+                        txt = infile.read()
+
+                    if version.parse(pd.__version__) >= version.parse("1.3.2"):
+                        x = pd.DataFrame(
+                            [[txt, labels[l]]], columns=["review", "sentiment"]
+                        )
+                        df = pd.concat([df, x], ignore_index=False)
+
+                    else:
+                        df = df.append([[txt, labels[l]]], ignore_index=True)
+                    pbar.update()
+    df.columns = ["text", "label"]
+
+    np.random.seed(0)
+    df = df.reindex(np.random.permutation(df.index))
+
+    print("Class distribution:")
+    np.bincount(df["label"].values)
+
+    return df
+
+
+def partition_dataset(df):
+    df_shuffled = df.sample(frac=1, random_state=1).reset_index()
+
+    df_train = df_shuffled.iloc[:35_000]
+    df_val = df_shuffled.iloc[35_000:40_000]
+    df_test = df_shuffled.iloc[40_000:]
+
+    df_train.to_csv("train.csv", index=False, encoding="utf-8")
+    df_val.to_csv("val.csv", index=False, encoding="utf-8")
+    df_test.to_csv("test.csv", index=False, encoding="utf-8")
+
+
+class IMDBDataset(Dataset):
+    def __init__(self, dataset_dict, partition_key="train"):
+        self.partition = dataset_dict[partition_key]
+
+    def __getitem__(self, index):
+        return self.partition[index]
+
+    def __len__(self):
+        return self.partition.num_rows
+
+def tokenize_text(batch):
+    return tokenizer(batch["text"], truncation=True, padding=True)
+
+
+def plot_logs(log_dir):
+    metrics = pd.read_csv(op.join(log_dir, "metrics.csv"))
+
+    aggreg_metrics = []
+    agg_col = "epoch"
+    for i, dfg in metrics.groupby(agg_col):
+        agg = dict(dfg.mean())
+        agg[agg_col] = i
+        aggreg_metrics.append(agg)
+
+    df_metrics = pd.DataFrame(aggreg_metrics)
+    df_metrics[["train_loss", "val_loss"]].plot(
+        grid=True, legend=True, xlabel="Epoch", ylabel="Loss"
+    )
+    plt.savefig(op.join(log_dir, "loss.pdf"))
+
+    df_metrics[["train_acc", "val_acc"]].plot(
+        grid=True, legend=True, xlabel="Epoch", ylabel="Accuracy"
+    )
+    plt.savefig(op.join(log_dir, "acc.pdf"))
+
+
+def train(num_epochs, model, optimizer, train_loader, val_loader, test_loader, fabric):
+
+    for epoch in range(num_epochs):
+        train_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2).to(fabric.device)
+
+        model.train()
+        for batch_idx, batch in enumerate(train_loader):
+            model.train()
+
+            #for s in ["input_ids", "attention_mask", "label"]:
+            #    batch[s] = batch[s].to(device)
+
+            ### FORWARD AND BACK PROP
+            outputs = model(batch["input_ids"], attention_mask=batch["attention_mask"], labels=batch["label"])
+            optimizer.zero_grad()
+            #outputs["loss"].backward()
+            fabric.backward(outputs["loss"])
+
+            ### UPDATE MODEL PARAMETERS
+            optimizer.step()
+
+            ### LOGGING
+            if not batch_idx % 300:
+                print(f"Epoch: {epoch+1:04d}/{num_epochs:04d} | Batch {batch_idx:04d}/{len(train_loader):04d} | Loss: {outputs['loss']:.4f}")
+
+            model.eval()
+            with torch.no_grad():
+                predicted_labels = torch.argmax(outputs["logits"], 1)
+                train_acc.update(predicted_labels, batch["label"])
+
+        ### MORE LOGGING
+        with torch.no_grad():
+            model.eval()
+            val_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2).to(fabric.device)
+            for batch in val_loader:
+                #for s in ["input_ids", "attention_mask", "label"]:
+                #    batch[s] = batch[s].to(device)
+                outputs = model(batch["input_ids"], attention_mask=batch["attention_mask"], labels=batch["label"])
+                predicted_labels = torch.argmax(outputs["logits"], 1)
+                val_acc.update(predicted_labels, batch["label"])
+
+            print(f"Epoch: {epoch+1:04d}/{num_epochs:04d} | Train acc.: {train_acc.compute()*100:.2f}% | Val acc.: {val_acc.compute()*100:.2f}%")
+
+if __name__ == "__main__":
+
+    print(watermark(packages="torch,lightning,transformers", python=True))
+    print("Torch CUDA available?", torch.cuda.is_available())
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    torch.manual_seed(123)
+
+    ##########################
+    ### 1 Loading the Dataset
+    ##########################
+    download_dataset()
+    df = load_dataset_into_to_dataframe()
+    if not (op.exists("train.csv") and op.exists("val.csv") and op.exists("test.csv")):
+        partition_dataset(df)
+
+    imdb_dataset = load_dataset(
+        "csv",
+        data_files={
+            "train": "train.csv",
+            "validation": "val.csv",
+            "test": "test.csv",
+        },
+    )
+
+    #########################################
+    ### 2 Tokenization and Numericalization
+    #########################################
+
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    print("Tokenizer input max length:", tokenizer.model_max_length, flush=True)
+    print("Tokenizer vocabulary size:", tokenizer.vocab_size, flush=True)
+
+    print("Tokenizing ...", flush=True)
+    imdb_tokenized = imdb_dataset.map(tokenize_text, batched=True, batch_size=None)
+    del imdb_dataset
+    imdb_tokenized.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    #########################################
+    ### 3 Set Up DataLoaders
+    #########################################
+
+    train_dataset = IMDBDataset(imdb_tokenized, partition_key="train")
+    val_dataset = IMDBDataset(imdb_tokenized, partition_key="validation")
+    test_dataset = IMDBDataset(imdb_tokenized, partition_key="test")
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=12,
+        shuffle=True,
+        num_workers=4,
+        drop_last=True,
+    )
+
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=12,
+        num_workers=2,
+        drop_last=True,
+    )
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=12,
+        num_workers=2,
+        drop_last=True,
+    )
+
+    #########################################
+    ### 4 Initializing the Model
+    #########################################
+
+    fabric = Fabric(accelerator="cuda", devices=4, strategy="ddp")
+    fabric.launch()
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "distilbert-base-uncased", num_labels=2)
+
+    # model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
+
+    model, optimizer = fabric.setup(model, optimizer)
+    train_loader, val_loader, test_loader = fabric.setup_dataloaders(train_loader, val_loader, test_loader)
+
+    #########################################
+    ### 5 Finetuning
+    #########################################
+
+    start = time.time()
+    train(
+        num_epochs=1,
+        model=model,
+        optimizer=optimizer,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        test_loader=test_loader,
+        fabric=fabric
+    )
+
+    end = time.time()
+    elapsed = end-start
+    print(f"Time elapsed {elapsed/60:.2f} min")
+
+    with torch.no_grad():
+        model.eval()
+        test_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2).to(fabric.device)
+        for batch in val_loader:
+            #for s in ["input_ids", "attention_mask", "label"]:
+            #    batch[s] = batch[s].to(device)
+            outputs = model(batch["input_ids"], attention_mask=batch["attention_mask"], labels=batch["label"])
+            predicted_labels = torch.argmax(outputs["logits"], 1)
+            test_acc.update(predicted_labels, batch["label"])
+
+    print(f"Test accuracy {test_acc.compute()*100:.2f}%")
