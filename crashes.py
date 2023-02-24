dcae81b9b27e77af04f61b05c2e118d975b891b3
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-02-24 16:19:06 +0100

    df

--- crashes.py
+++ crashes.py
@@ -266,16 +266,11 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         model.eval()
-        test_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2).to(device)
         for idx, batch in enumerate(test_loader):
             for s in ["input_ids", "attention_mask", "label"]:
                batch[s] = batch[s].to(device)
             outputs = model(batch["input_ids"], attention_mask=batch["attention_mask"], labels=batch["label"])
             predicted_labels = torch.argmax(outputs["logits"], 1)
-
             print("rank", local_rank, "update test_acc", idx)
-            # test_acc.update(predicted_labels, batch["label"])
-            print("rank", local_rank, "update test_acc done", idx)
 
     torch.distributed.barrier()
-    # print(f"Test accuracy {test_acc.compute()*100:.2f}%")
