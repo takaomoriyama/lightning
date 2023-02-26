724ed89223a3d25991ba5bc2ceb6f50849b46b60
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-02-26 22:52:22 +0100

    trainer

---
+++ crashes_trainer.py
@@ -0,0 +1,260 @@
+"""
+
+"""
+import os.path as op
+from copy import deepcopy
+
+from datasets import load_dataset
+from torch.nn.parallel import DistributedDataParallel
+
+import torch
+from torch.utils.data import DataLoader
+import torchmetrics
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+
+
+import os
+import sys
+import tarfile
+import time
+
+import numpy as np
+import pandas as pd
+from packaging import version
+from torch.utils.data import Dataset
+from tqdm import tqdm
+import urllib
+
+from lightning import LightningModule, Trainer
+
+
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = progress_size / (1024.0**2 * duration)
+    percent = count * block_size * 100.0 / total_size
+
+    sys.stdout.write(
+        f"\r{int(percent)}% | {progress_size / (1024.**2):.2f} MB "
+        f"| {speed:.2f} MB/s | {duration:.2f} sec elapsed"
+    )
+    sys.stdout.flush()
+
+
+def download_dataset():
+    source = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+    target = "aclImdb_v1.tar.gz"
+
+    if os.path.exists(target):
+        os.remove(target)
+
+    if not os.path.isdir("aclImdb") and not os.path.isfile("aclImdb_v1.tar.gz"):
+        urllib.request.urlretrieve(source, target, reporthook)
+
+    if not os.path.isdir("aclImdb"):
+
+        with tarfile.open(target, "r:gz") as tar:
+            tar.extractall()
+
+
+def load_dataset_into_to_dataframe():
+    basepath = "aclImdb"
+
+    labels = {"pos": 1, "neg": 0}
+
+    df = pd.DataFrame()
+
+    with tqdm(total=50000) as pbar:
+        for s in ("test", "train"):
+            for l in ("pos", "neg"):
+                path = os.path.join(basepath, s, l)
+                for file in sorted(os.listdir(path)):
+                    with open(os.path.join(path, file), "r", encoding="utf-8") as infile:
+                        txt = infile.read()
+
+                    if version.parse(pd.__version__) >= version.parse("1.3.2"):
+                        x = pd.DataFrame(
+                            [[txt, labels[l]]], columns=["review", "sentiment"]
+                        )
+                        df = pd.concat([df, x], ignore_index=False)
+
+                    else:
+                        df = df.append([[txt, labels[l]]], ignore_index=True)
+                    pbar.update()
+    df.columns = ["text", "label"]
+
+    np.random.seed(0)
+    df = df.reindex(np.random.permutation(df.index))
+
+    print("Class distribution:")
+    np.bincount(df["label"].values)
+
+    return df
+
+
+def partition_dataset(df):
+    df_shuffled = df.sample(frac=1, random_state=1).reset_index()
+
+    df_train = df_shuffled.iloc[:35_000]
+    df_test = df_shuffled.iloc[40_000:]
+
+    df_train.to_csv("train.csv", index=False, encoding="utf-8")
+    df_test.to_csv("test.csv", index=False, encoding="utf-8")
+
+
+class IMDBDataset(Dataset):
+    def __init__(self, dataset_dict, partition_key="train"):
+        self.partition = dataset_dict[partition_key]
+
+    def __getitem__(self, index):
+        return self.partition[index]
+
+    def __len__(self):
+        return self.partition.num_rows
+
+
+def tokenize_text(batch):
+    return tokenizer(batch["text"], truncation=True, padding=True)
+
+
+class LightningModel(LightningModule):
+    def __init__(self, model, learning_rate=5e-5):
+        super().__init__()
+
+        self.learning_rate = learning_rate
+        self.model = model
+
+        self.train_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2)
+        self.val_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2)
+        self.test_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2)
+
+    def forward(self, input_ids, attention_mask, labels):
+        return self.model(input_ids, attention_mask=attention_mask, labels=labels)
+
+    def training_step(self, batch, batch_idx):
+        outputs = self(batch["input_ids"], attention_mask=batch["attention_mask"],
+                       labels=batch["label"])        
+        self.log("train_loss", outputs["loss"])
+        with torch.no_grad():
+            logits = outputs["logits"]
+            predicted_labels = torch.argmax(logits, 1)
+            self.train_acc(predicted_labels, batch["label"])
+            self.log("train_acc", self.train_acc, on_epoch=True, on_step=False)
+        return outputs["loss"]  # this is passed to the optimizer for training
+
+    def validation_step(self, batch, batch_idx):
+        outputs = self(batch["input_ids"], attention_mask=batch["attention_mask"],
+                       labels=batch["label"])        
+        self.log("val_loss", outputs["loss"], prog_bar=True)
+
+        logits = outputs["logits"]
+        predicted_labels = torch.argmax(logits, 1)
+        self.val_acc(predicted_labels, batch["label"])
+        self.log("val_acc", self.val_acc, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        outputs = self(batch["input_ids"], attention_mask=batch["attention_mask"],
+                       labels=batch["label"])        
+
+        logits = outputs["logits"]
+        predicted_labels = torch.argmax(logits, 1)
+        self.test_acc(predicted_labels, batch["label"])
+        self.log("accuracy", self.test_acc, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.trainer.model.parameters(), lr=self.learning_rate)
+        return optimizer
+
+if __name__ == "__main__":
+    torch.manual_seed(123)
+
+    ##########################
+    ### 1 Loading the Dataset
+    ##########################
+    download_dataset()
+    df = load_dataset_into_to_dataframe()
+    if not (op.exists("train.csv") and op.exists("val.csv") and op.exists("test.csv")):
+        partition_dataset(df)
+
+    imdb_dataset = load_dataset(
+        "csv",
+        data_files={
+            "train": "train.csv",
+            "validation": "val.csv",
+            "test": "test.csv",
+        },
+    )
+
+    #########################################
+    ### 2 Tokenization and Numericalization
+    ########################################
+
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    print("Tokenizer input max length:", tokenizer.model_max_length, flush=True)
+    print("Tokenizer vocabulary size:", tokenizer.vocab_size, flush=True)
+
+    print("Tokenizing ...", flush=True)
+    imdb_tokenized = imdb_dataset.map(tokenize_text, batched=True, batch_size=None)
+    del imdb_dataset
+    imdb_tokenized.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    #########################################
+    ### 3 Set Up DataLoaders
+    #########################################
+
+    train_dataset = IMDBDataset(imdb_tokenized, partition_key="train")
+    val_dataset = IMDBDataset(imdb_tokenized, partition_key="validation")
+    test_dataset = IMDBDataset(imdb_tokenized, partition_key="test")
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=12,
+        shuffle=True, 
+        num_workers=4
+    )
+
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=12,
+        num_workers=4
+    )
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=12,
+        num_workers=4
+    )
+    #########################################
+    ### 4 Initializing the Model
+    #########################################
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "distilbert-base-uncased", num_labels=2)
+    #########################################
+    ### 5 Finetuning
+    #########################################
+    lightning_model = LightningModel(model)
+    trainer = Trainer(
+        max_epochs=3,
+        precision="16",
+        accelerator="gpu",
+        strategy="ddp",
+        devices=2,
+    )
+    start = time.time()
+    trainer.fit(model=lightning_model,
+                train_dataloaders=train_loader,
+                val_dataloaders=val_loader)
+    end = time.time()
+    elapsed = end-start
+    print(f"Time elapsed {elapsed/60:.2f} min")
+    test_acc = trainer.test(lightning_model, dataloaders=test_loader, ckpt_path="best")
+    print(test_acc)
+    with open(op.join(trainer.logger.log_dir, "outputs.txt"), "w") as f:
+        f.write((f"Time elapsed {elapsed/60:.2f} min\n"))
+        f.write(f"Test acc: {test_acc}")
