76901bccfe958fbd70cfa225034ec31443c397fa
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-02-27 02:14:52 +0100

    reset

--- crashes.py
+++ crashes.py
@@ -112,8 +112,9 @@ def train(num_epochs, model, optimizer, train_loader, device):
         print(train_acc._defaults)
         for attr, default in train_acc._defaults.items():
             current_val = getattr(train_acc, attr)
-            # if isinstance(default, torch.Tensor):
-            #     setattr(train_acc, attr, default.detach().clone().to(current_val.device))
+            print(current_val.device)
+            if isinstance(default, torch.Tensor):
+                setattr(train_acc, attr, default.detach().clone().to(current_val.device))
             # else:
             #     setattr(train_acc, attr, [])
 
