b8a26b87cadf69cc5f0412e4e645314c707a18e2
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-02-27 01:55:19 +0100

    reset

--- crashes.py
+++ crashes.py
@@ -46,8 +46,6 @@ Traceback (most recent call last):
 RuntimeError: DataLoader worker (pid 2339965) is killed by signal: Aborted.
 
 """
-import os.path as op
-from copy import deepcopy
 
 from datasets import load_dataset
 from torch.nn.parallel import DistributedDataParallel
@@ -61,15 +59,9 @@ from transformers import AutoModelForSequenceClassification
 
 import os
 import sys
-import tarfile
 import time
 
-import numpy as np
-import pandas as pd
-from packaging import version
 from torch.utils.data import Dataset
-from tqdm import tqdm
-import urllib
 
 
 def reporthook(count, block_size, total_size):
@@ -86,61 +78,6 @@ def reporthook(count, block_size, total_size):
         f"\r{int(percent)}% | {progress_size / (1024.**2):.2f} MB " f"| {speed:.2f} MB/s | {duration:.2f} sec elapsed"
     )
     sys.stdout.flush()
-
-
-def download_dataset():
-    source = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
-    target = "aclImdb_v1.tar.gz"
-
-    if os.path.exists(target):
-        os.remove(target)
-
-    if not os.path.isdir("aclImdb") and not os.path.isfile("aclImdb_v1.tar.gz"):
-        urllib.request.urlretrieve(source, target, reporthook)
-
-    if not os.path.isdir("aclImdb"):
-
-        with tarfile.open(target, "r:gz") as tar:
-            tar.extractall()
-
-
-def load_dataset_into_to_dataframe():
-    basepath = "aclImdb"
-
-    labels = {"pos": 1, "neg": 0}
-
-    df = pd.DataFrame()
-
-    with tqdm(total=50000) as pbar:
-        for s in ("test", "train"):
-            for l in ("pos", "neg"):
-                path = os.path.join(basepath, s, l)
-                for file in sorted(os.listdir(path)):
-                    with open(os.path.join(path, file), "r", encoding="utf-8") as infile:
-                        txt = infile.read()
-
-                    if version.parse(pd.__version__) >= version.parse("1.3.2"):
-                        x = pd.DataFrame([[txt, labels[l]]], columns=["review", "sentiment"])
-                        df = pd.concat([df, x], ignore_index=False)
-
-                    else:
-                        df = df.append([[txt, labels[l]]], ignore_index=True)
-                    pbar.update()
-    df.columns = ["text", "label"]
-
-    np.random.seed(0)
-    df = df.reindex(np.random.permutation(df.index))
-    return df
-
-
-def partition_dataset(df):
-    df_shuffled = df.sample(frac=1, random_state=1).reset_index()
-
-    df_train = df_shuffled.iloc[:35_000]
-    df_test = df_shuffled.iloc[40_000:]
-
-    df_train.to_csv("train.csv", index=False, encoding="utf-8")
-    df_test.to_csv("test.csv", index=False, encoding="utf-8")
 
 
 class IMDBDataset(Dataset):
@@ -198,15 +135,6 @@ if __name__ == "__main__":
     torch.cuda.set_device(local_rank)
 
     torch.distributed.init_process_group("nccl", rank=local_rank, world_size=2)
-
-    if local_rank == 0:
-        download_dataset()
-
-    torch.distributed.barrier()
-
-    df = load_dataset_into_to_dataframe()
-    if not (op.exists("train.csv") and op.exists("val.csv") and op.exists("test.csv")):
-        partition_dataset(df)
 
     imdb_dataset = load_dataset(
         "csv",
