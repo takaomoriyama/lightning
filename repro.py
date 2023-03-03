a26ce821297c20451abf5fcabda5f9607779b41c
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-03-03 10:30:30 +0100

    debug

--- repro.py
+++ repro.py
@@ -5,19 +5,19 @@ import torch.nn as nn
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.distributed.fsdp import FullyShardedDataParallel
+from torch.nn.parallel import DistributedDataParallel
 
 
 def work(rank):
     os.environ["MASTER_ADDR"] = "127.0.0.1"
     os.environ["MASTER_PORT"] = "1234"
     dist.init_process_group("nccl", world_size=2, rank=rank)
-    print("hello")
 
     torch.cuda.set_device(rank)
     device = torch.device("cuda", rank)
 
     model = nn.Linear(100, 50).to(device)
-    model = FullyShardedDataParallel(model)
+    model = DistributedDataParallel(model, device_ids=[rank])
 
     with torch.inference_mode():
         model(torch.rand(2, 100))
@@ -26,6 +26,7 @@ def work(rank):
 
     with torch.inference_mode():
         model.load_state_dict(torch.load("fsdp_model.pt"))
+        model(torch.rand(2, 100))
 
 
 def run():
