57bb40e5285b2f55bbbe03650de4d9091cdad8aa
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-03-21 15:33:32 +0100

    debug

--- examples/pytorch/bug_report/bug_report_model.py
+++ examples/pytorch/bug_report/bug_report_model.py
@@ -3,11 +3,11 @@ import os
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader, random_split, Dataset
+from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import MNIST
 from torchvision import transforms
 tmpdir = os.getcwd()
-from lightning import Trainer, LightningModule, LightningDataModule
+from lightning import Trainer, LightningModule
 
 PATH_DATASETS = os.environ.get('PATH_DATASETS', '.')
 BATCH_SIZE = 256
@@ -28,13 +28,15 @@ class LitMNIST(LightningModule):
         # Hardcode some dataset specific attributes
         self.num_classes = 10
         self.dims = (1, 28, 28)
-        channels, width, height = self.dims
+
         self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307, ), (0.3081, ))])
 
+    def configure_sharded_model(self) -> None:
+        channels, width, height = self.dims
         # Define PyTorch model
         self.model = nn.Sequential(
-            nn.Flatten(), nn.Linear(channels * width * height, hidden_size), nn.ReLU(), nn.Dropout(0.1),
-            nn.Linear(hidden_size, hidden_size), nn.ReLU(), nn.Dropout(0.1), nn.Linear(hidden_size, self.num_classes)
+            nn.Flatten(), nn.Linear(channels * width * height, self.hidden_size), nn.ReLU(), nn.Dropout(0.1),
+            nn.Linear(self.hidden_size, self.hidden_size), nn.ReLU(), nn.Dropout(0.1), nn.Linear(self.hidden_size, self.num_classes)
         )
 
     def forward(self, x):
@@ -51,7 +53,6 @@ class LitMNIST(LightningModule):
         x, y = batch
         logits = self(x)
         loss = F.nll_loss(logits, y)
-        preds = torch.argmax(logits, dim=1)
 
         # Calling self.log will surface up scalars for you in TensorBoard
         self.log('val_loss', loss, prog_bar=True)
@@ -62,7 +63,7 @@ class LitMNIST(LightningModule):
         return self.validation_step(batch, batch_idx)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
         return optimizer
 
     ####################
@@ -95,46 +96,13 @@ class LitMNIST(LightningModule):
         return DataLoader(self.mnist_test, batch_size=BATCH_SIZE)
 
 
-class MyDataModule(LightningDataModule):
-    def __init__(self, data_dir=PATH_DATASETS):
-        super().__init__()
-        self.data_dir = data_dir
-        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307, ), (0.3081, ))])
-
-    def prepare_data(self):
-        # download
-        MNIST(self.data_dir, train=True, download=True)
-        MNIST(self.data_dir, train=False, download=True)
-
-    def setup(self, stage=None):
-
-        # Assign train/val datasets for use in dataloaders
-        if stage == 'fit' or stage is None:
-            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
-            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
-
-        # Assign test dataset for use in dataloader(s)
-        if stage == 'test' or stage is None:
-            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
-
-    def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=BATCH_SIZE)
-
-    def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=BATCH_SIZE)
-
-    def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=BATCH_SIZE)
-
-
 if __name__ == '__main__':
     model = LitMNIST()
-    datamodule = MyDataModule()
     trainer = Trainer(
         devices=[2, 3],
         accelerator="cuda",
         max_epochs=1,
         precision=16,
-        strategy="deepspeed_stage_2"
+        strategy="deepspeed_stage_3"
     )
-    trainer.fit(model, datamodule)
+    trainer.fit(model)
