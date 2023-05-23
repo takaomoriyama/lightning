82031ac08fd5d1ee08ead4dce21e975f82d8f71d
Jirka B <j.borovec+github@gmail.com>
2023-05-23 16:10:18 -0400

    import

--- src/lightning/pytorch/utilities/__init__.py
+++ src/lightning/pytorch/utilities/__init__.py
@@ -17,6 +17,7 @@ import numpy
 from lightning_utilities.core.apply_func import apply_to_collection  # noqa: F401
 from lightning_utilities.core.enums import StrEnum as LightningEnum  # noqa: F401
 
+from lightning.fabric.utilities.apply_func import move_data_to_device  # noqa: F401
 from lightning.pytorch.utilities.combined_loader import CombinedLoader  # noqa: F401
 from lightning.pytorch.utilities.enums import GradClipAlgorithmType  # noqa: F401
 from lightning.pytorch.utilities.grads import grad_norm  # noqa: F401
